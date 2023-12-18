08095e3cf6b67e446b4cf4adb650d399783d0d7d
camper <charles.amper@tpx.com>
2023-12-18 19:40:15 +0800

    fix css issue and version

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import "./bootstrap.superhero.min.css";
 import './index.css';
 
 import App from './App';
@@ -9,8 +10,9 @@ import * as serviceWorker from './serviceWorker';
 import configStore from './redux/store';
 import { Provider } from 'react-redux';
 
-import "bootstrap/dist/css/bootstrap.superhero.min.css";
+//import "bootstrap/dist/css/bootstrap.superhero.min.css";
 
+//
 const store = configStore();
 
 ReactDOM.render(
